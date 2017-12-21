c56193e17c953ea7b91cfd657c6a964b2550818b
gormanjp <jpgorman8@gmail.com>
2017-12-21 12:17:19 -0500

    fetch name info from api

--- app/components/App.js
+++ app/components/App.js
@@ -1,9 +1,34 @@
 import React, {Component} from 'react';
+import { NameCard } from './NameCard';
+
+const nameApi = 'https://willowtreeapps.com/api/v1.0/profiles/';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {};
+  }
+
+  // fetch all of the names from the wt profile api once the main component mounts
+  componentDidMount(){
+    fetch(nameApi)
+      .then(names => names.json())
+      .then(names => {
+        this.setState({
+          nameInfo: names
+        })
+      })
+  };
+
   render(){
+    if(!this.state.nameInfo) {
+      return <div> Loading... </div>;
+    }
     return(
-      <div className="">This is the real deal name game</div>
+      <div>
+        <div className="">This is the real deal name game</div>
+        <NameCard />
+      </div>
     )
   }
 }
---
+++ app/components/NameCard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const NameCard = () => {
+  return(
+    <div className="card">
+      <div className="card-body">
+        This is some text within a card body.
+      </div>
+    </div>
+  )
+}
