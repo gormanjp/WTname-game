eabaea4028521421e5ce7b8e3977d84d4d7b9d04
gormanjp <jpgorman8@gmail.com>
2017-12-21 11:31:37 -0500

    initial react, webpack, babel setup

---
+++ app/components/App.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class App extends Component {
+  render(){
+    return(
+      <div className="">This is the real deal name game</div>
+    )
+  }
+}
+
+export default App;
